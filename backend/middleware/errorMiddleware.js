d5cf815d89647f676acb72230bb682d16cc35178
Philip Simpson <dev.phits@gmail.com>
2022-09-20 13:30:23 -0700

    Add error handler

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,11 +1,9 @@
-const { stat } = require('fs');
-
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500
     res.status(statusCode)
     res.json({
         message: err.message,
-        stack: prcoess.env.NODE_ENV === 'production' ? null : err.stack
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
     })
 }
 
