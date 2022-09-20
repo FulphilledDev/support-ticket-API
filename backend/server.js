36f3a46510807716003f48a8481aa5e9852929ce
Philip Simpson <dev.phits@gmail.com>
2022-09-20 13:20:44 -0700

    Add user Route, Error Middleware, User Controller

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('Server...')
+const express = require('express');
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+app.get('/', (req, res) => {
+    res.status(200).json('Welcome to the support desk API')
+})
+
+// Routes
+app.use('/api/users', require('./routes/users'))
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
