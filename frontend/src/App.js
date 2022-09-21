5746bad5f6246e341e1300436afbdffb09e0ad50
Philip Simpson <dev.phits@gmail.com>
2022-09-20 18:00:46 -0700

    Add Forms to Home, Login, and Register Pages w/ Icons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -6,6 +8,7 @@ import Register from './pages/Register';
 
 function App() {
   return (
+    <>
     <Router>
       <div className='container'>
         <Header />
@@ -16,6 +19,8 @@ function App() {
         </Routes>
       </div> 
     </Router>
+    <ToastContainer />
+    </>
   );
 }
 
