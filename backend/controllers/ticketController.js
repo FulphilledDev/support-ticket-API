cfdf85716a08694c0d0ab0a3b8bd5ec77a8c3490
Philip Simpson <dev.phits@gmail.com>
2022-09-24 09:41:56 -0700

    Backend: Create and Get Tickets

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -6,17 +6,50 @@ const Ticket = require('../models/ticketModel')
 // @desc Get user tickets
 // @route GET /api/tickets/me
 // @access Private
-const getTickets = asyncHandler( async (req, res) => {
+const getTickets = asyncHandler(async (req, res) => {
     
-    res.status(200).json({message: 'getTickets'})
+    // Get user with JWT
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    // Then get tickets
+    const tickets = await Ticket.find({ user: req.user.id})
+    
+    res.status(200).json(tickets)
 })
 
 // @desc Create new ticket
 // @route POST /api/tickets
 // @access Private
-const createTicket = asyncHandler( async (req, res) => {
+const createTicket = asyncHandler(async (req, res) => {
     
-    res.status(200).json({message: 'createTicket'})
+    const { product, description } = req.body
+
+    if (!product || !description) {
+        res.status(400)
+        throw new Error('Please add a product and description')
+    }
+
+    // Get user with JWT
+    const user = await User.findById(req.user.id)
+
+    if (!user) {
+        res.status(401)
+        throw new Error('User not found')
+    }
+
+    const ticket = await Ticket.create({
+        product,
+        description,
+        user: req.user.id,
+        status: 'new'
+    })
+    
+    res.status(201).json(ticket)
 })
 
 
