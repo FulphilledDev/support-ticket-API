8a92b1a3865fb60373493f5bb649d5cb3ddbaae2
Philip Simpson <dev.phits@gmail.com>
2022-09-26 00:18:03 -0700

    src > pages - create Ticket page

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,4 +1,27 @@
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
+import { useSelector, useDispatch } from 'react-redux'
+import { getTicket, reset } from '../features/tickets/ticketSlice'
+import { useParams } from 'react-router-dom'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+
 function Ticket() {
+    const { ticket, isLoading, isSuccess, isError, message } = useSelector((state) => state.tickets)
+
+    const params = useParams()
+    const dispatch = useDispatch()
+    // useParams allows us to grab the ticketId from the URL
+    const { ticketId } = useParams()
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message)
+        }
+
+        dispatch(getTicket(ticketId))
+    }, [ isError, message, ticketId])
+
   return (
     <div>
       Ticket
